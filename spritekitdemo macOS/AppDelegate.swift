77e9c408aba3920fed2b9d7b51516394acf3aa49
masonpaisley <masonpaisleyy@gmail.com>
2023-04-26 11:51:25 +0800

    Initial Commit

---
+++ spritekitdemo macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  spritekitdemo macOS
+//
+//  Created by jinzhao wang on 2023/4/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
